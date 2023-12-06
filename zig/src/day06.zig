7198fc93d4aa2f71f20abcd9cdca81609a62239e
lockbox <lockbox.06@protonmail.com>
2023-12-06 12:22:55 -0500

    Day 6

--- zig/src/day06.zig
+++ zig/src/day06.zig
@@ -8,10 +8,152 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day06.txt");
+const DATA = @embedFile("data/day06.txt");
+const TEST_DATA =
+    \\Time:      7  15   30
+    \\Distance:  9  40  200
+;
+
+const Race = struct {
+    time: usize,
+    distance: usize,
+};
+
+const ValidHoldRange = struct {
+    start: usize = 1,
+    end: usize,
+};
+
+inline fn validHoldTimes(time: usize) ValidHoldRange {
+    return .{ .start = 1, .end = time };
+}
+
+fn getPart1Races(data: []const u8) !std.MultiArrayList(Race) {
+    var races = std.MultiArrayList(Race){};
+
+    var iterator = tokenizeSca(u8, data, '\n');
+    var line_time_slice = iterator.next().?;
+    var line_dist_slice = iterator.next().?;
+
+    const time_line_start = 1 + indexOf(u8, line_time_slice, ':').?;
+    const dist_line_start = 1 + indexOf(u8, line_dist_slice, ':').?;
+
+    var time_iterator = tokenizeSca(u8, line_time_slice[time_line_start..], ' ');
+    var dist_iterator = tokenizeSca(u8, line_dist_slice[dist_line_start..], ' ');
+
+    while (time_iterator.next()) |time_slice| {
+        const dist_slice = dist_iterator.next().?;
+
+        const time = try parseInt(usize, time_slice, 10);
+        const dist = try parseInt(usize, dist_slice, 10);
+
+        try races.append(gpa, Race{
+            .time = time,
+            .distance = dist,
+        });
+    }
+
+    return races;
+}
+
+inline fn holdTimeToDist(hold: usize, time: usize) usize {
+    const go_time = time - hold;
+
+    return go_time * hold;
+}
+
+fn part1(data: []const u8) !usize {
+    var races = try getPart1Races(data);
+    defer races.deinit(gpa);
+
+    var total_winning: usize = 1;
+
+    for (races.items(.time), races.items(.distance)) |time, dist| {
+        const time_range = validHoldTimes(time);
+        var winning: usize = 0;
+
+        for (time_range.start..time_range.end) |hold_time| {
+            const travelled = holdTimeToDist(hold_time, time);
+
+            if (travelled > dist) {
+                winning += 1;
+            }
+        }
+
+        total_winning *= winning;
+    }
+
+    return total_winning;
+}
+
+fn getPart2Races(data: []const u8) !std.MultiArrayList(Race) {
+    var races = std.MultiArrayList(Race){};
+
+    var iterator = tokenizeSca(u8, data, '\n');
+    var line_time_slice = iterator.next().?;
+    var line_dist_slice = iterator.next().?;
+
+    const time_line_start = 1 + indexOf(u8, line_time_slice, ':').?;
+    const dist_line_start = 1 + indexOf(u8, line_dist_slice, ':').?;
+
+    var time_iterator = tokenizeSca(u8, line_time_slice[time_line_start..], ' ');
+    var dist_iterator = tokenizeSca(u8, line_dist_slice[dist_line_start..], ' ');
+
+    var total_time: usize = 0;
+    var total_dist: usize = 0;
+    while (time_iterator.next()) |time_slice| {
+        const dist_slice = dist_iterator.next().?;
+
+        const time = try parseInt(usize, time_slice, 10);
+        const dist = try parseInt(usize, dist_slice, 10);
+
+        const time_size = time_slice.len;
+        const dist_size = dist_slice.len;
+
+        total_time *= std.math.pow(usize, 10, time_size);
+        total_dist *= std.math.pow(usize, 10, dist_size);
+
+        total_time += time;
+        total_dist += dist;
+    }
+
+    try races.append(gpa, Race{
+        .time = total_time,
+        .distance = total_dist,
+    });
+
+    return races;
+}
+
+fn part2(data: []const u8) !usize {
+    var races = try getPart2Races(data);
+    defer races.deinit(gpa);
+
+    var total_winning: usize = 1;
+
+    for (races.items(.time), races.items(.distance)) |time, dist| {
+        const time_range = validHoldTimes(time);
+        var winning: usize = 0;
+
+        for (time_range.start..time_range.end) |hold_time| {
+            const travelled = holdTimeToDist(hold_time, time);
+
+            if (travelled > dist) {
+                winning += 1;
+            }
+        }
+
+        total_winning *= winning;
+    }
+
+    return total_winning;
+}
 
 pub fn main() !void {
-    
+    print("[Test Data] Part1: `{}`\n", .{try part1(TEST_DATA)});
+    print("[Real Data] Part1: `{}`\n", .{try part1(DATA)});
+    print("[Test Data] Part2: `{}`\n", .{try part2(TEST_DATA)});
+    print("[Real Data] Part2: `{}`\n", .{try part2(DATA)});
 }
 
 // Useful stdlib functions
