93cda92e7b1662485b568ee073cb7b61dfb76a66
lockbox <lockbox.06@protonmail.com>
2023-12-09 11:28:34 -0500

    day 9

--- zig/src/day09.zig
+++ zig/src/day09.zig
@@ -8,10 +8,108 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day09.txt");
+const DATA = @embedFile("data/day09.txt");
+const TEST_DATA =
+    \\0 3 6 9 12 15
+    \\1 3 6 10 15 21
+    \\10 13 16 21 30 45
+;
+
+fn calculateNextRecursive(xs: *List(isize)) isize {
+    // check for base case (all == 0)
+    for (xs.items) |x| {
+        if (x != 0) break;
+    } else {
+        return 0;
+    }
+    // normal case, make a list of the derivative
+    var ys = List(isize).init(gpa);
+    defer ys.deinit();
+
+    var idx: usize = 1;
+    while (idx < xs.items.len) {
+        const delta = xs.items[idx] - xs.items[idx - 1];
+        ys.append(delta) catch @panic("oom");
+
+        // inc
+        idx += 1;
+    }
+
+    return ys.items[ys.items.len - 1] + calculateNextRecursive(&ys);
+}
+
+fn part1(data: []const u8) !isize {
+    var iterator = tokenizeSca(u8, data, '\n');
+    var sum: isize = 0;
+
+    while (iterator.next()) |line| {
+        var numbers = List(isize).init(gpa);
+        defer numbers.deinit();
+
+        var num_iterator = tokenizeSca(u8, line, ' ');
+        while (num_iterator.next()) |num_ascii| {
+            try numbers.append(try parseInt(isize, num_ascii, 10));
+        }
+
+        // now calulate the next item in the sequence
+        const line_next = calculateNextRecursive(&numbers);
+        sum += line_next + numbers.items[numbers.items.len - 1];
+    }
+
+    return sum;
+}
+
+fn calculatePrevRecursive(xs: *List(isize)) isize {
+    // check for base case (all == 0)
+    for (xs.items) |x| {
+        if (x != 0) break;
+    } else {
+        return 0;
+    }
+
+    // normal case, make a list of the derivative
+    var ys = List(isize).init(gpa);
+    defer ys.deinit();
+
+    var idx: usize = xs.items.len - 1;
+    while (idx > 0) {
+        const delta = xs.items[idx] - xs.items[idx - 1];
+        ys.insert(0, delta) catch @panic("oom");
+
+        // inc
+        idx -= 1;
+    }
+
+    return ys.items[0] - calculatePrevRecursive(&ys);
+}
+
+fn part2(data: []const u8) !isize {
+    var iterator = tokenizeSca(u8, data, '\n');
+    var sum: isize = 0;
+
+    while (iterator.next()) |line| {
+        var numbers = List(isize).init(gpa);
+        defer numbers.deinit();
+
+        var num_iterator = tokenizeSca(u8, line, ' ');
+        while (num_iterator.next()) |num_ascii| {
+            const num = try parseInt(isize, num_ascii, 10);
+            try numbers.append(num);
+        }
+
+        // now calulate the next item in the sequence
+        const line_prev = calculatePrevRecursive(&numbers);
+        sum += numbers.items[0] - line_prev;
+    }
+
+    return sum;
+}
 
 pub fn main() !void {
-    
+    print("[Test Data]: Part1: `{}`\n", .{try part1(TEST_DATA)});
+    print("[Real Data]: Part1: `{}`\n", .{try part1(DATA)});
+    print("[Test Data]: Part2: `{}`\n", .{try part2(TEST_DATA)});
+    print("[Real Data]: Part2: `{}`\n", .{try part2(DATA)});
 }
 
 // Useful stdlib functions
